665b05ebe9916b81efa48b5a811c3825b3c24fd8
Omelia-Hamilton <omeliah128@gmail.com>
2024-11-19 12:16:09 -0500

    Added Initial starter files

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+   
+   document.addEventListener("DOMContentLoaded", function () {
    document.getElementById("searchButton").addEventListener("click", function() {
-     const httpRequest = new XMLHttpRequest();
-     let url = "http://localhost/info2180-lab4/superheroes.php";
+    console.log("Button clicked!")
+    const searchField = document.getElementById("searchField");
+    const resultDiv = document.getElementById("result");
+    const searchQuery = searchField.value.trim(); // Get and trim the input 
+    const httpRequest = new XMLHttpRequest();
+    let url = `http://localhost/info2180-lab4/superheroes.php?query=${query}`;
+
+
+     if (!searchQuery) {
+        resultDiv.innerHTML = "Please enter a superhero name or alias."; //validate input
+        return;
+    }
  
      // Set the onreadystatechange callback properly
      httpRequest.onreadystatechange = function() {
@@ -10,8 +22,9 @@
                  let response = httpRequest.responseText;
                  alert(response);
              } else {
+                resultDiv.innerHTML = response; // Display response in the result div
                  // Handle errors
-                 alert('There was a problem with the request.');
+                 resultDiv.innerHTML = "There was a problem with the request.";
              }
          }
      };
@@ -19,6 +32,6 @@
      // Open and send the GET request
      httpRequest.open('GET', url, true);
      httpRequest.send();
- });
- 
- 
+
+
+   })});
